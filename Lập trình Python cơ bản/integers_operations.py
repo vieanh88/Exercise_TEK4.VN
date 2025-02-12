c0ae0f0c84a5918817817c51efd096b01f629504
vieanh88 <vietanh88.hust@gmail.com>
2025-02-12 14:32:08 +0700

    update

---
+++ Lập trình Python cơ bản/integers_operations.py
@@ -0,0 +1,37 @@
+# basic operations on integers
+# Bài tập
+## Hãy viết chương trình Python hiển thị ra màn hình tổng, hiệu, tích, thương, kết quả của phép chia nguyên, phép chia dư của 99358 và 1254.
+a = 99358
+b = 1254
+
+addition = a+b
+subtraction = a-b
+multiplication = a*b 
+division = a/b 
+quotient = a//b
+remainder = a%b
+
+print(addition)
+print(subtraction)
+print(multiplication)
+print(division)
+print(quotient)
+print(remainder)
+
+# basic operations on integers 2
+# Bài tập
+## Hãy viết chương trình Python để hiển thị ra màn hình tổng, hiệu, tích và thương và số dư của phép chia giữa hai số 3586 và 952 dưới dạng yêu cầu.
+a = 3586
+b = 952
+
+KQ1 = a+b
+KQ2 = a-b
+KQ3 = a*b
+KQ4 = a/b
+KQ5 = a%b
+
+print('Tổng của', a, '+', b, 'là:', KQ1)
+print('Hiệu của', a, '-', b, 'là:', KQ2)
+print('Tích của', a, '*', b, 'là:', KQ3)
+print('Thương của', a, '/', b, 'là:', KQ4)
+print(a, 'chia', b, 'dư là:', KQ5)
